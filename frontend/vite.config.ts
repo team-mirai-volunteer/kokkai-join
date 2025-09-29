2e8bdadaef00276a276dadddb3d910fa62cad3f7
skanehira <sho19921005@gmail.com>
2025-09-29 23:33:10 +0900

    fix: ci

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import process from "node:process";
 
 // https://vite.dev/config/
 export default defineConfig({
