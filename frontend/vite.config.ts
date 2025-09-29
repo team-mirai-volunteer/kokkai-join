de99c587cccf5f4c92b29a9bcddfd00f42f91092
skanehira <sho19921005@gmail.com>
2025-09-29 23:23:07 +0900

    fix: base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import process from "node:process";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.VITE_BASE_PATH || "/",
+});
